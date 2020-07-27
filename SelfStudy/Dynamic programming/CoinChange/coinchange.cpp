11f91bfcd90d2b44961fa70563d38bdecbac281b
MinHie <55249566+leminhhieust@users.noreply.github.com>
2020-07-27 10:40:36 +0700

    update

---
+++ SelfStudy/Dynamic programming/CoinChange/coinchange.cpp
@@ -0,0 +1,11 @@
+// Programmer: Min Hie
+// Last Modified: 27/07/2020
+// Functional description: 
+// Use dynamic programming to find longest increasing 
+// subsequence of array of N elements
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void solve_bottom_up();
