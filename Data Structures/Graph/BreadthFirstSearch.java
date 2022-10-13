582db6e1a2de36aafc7cc61342ba353f853eebb7
hieulm2k <leminhhhieust@gmail.com>
2022-10-13 22:05:06 +0700

    Graph: BFS

---
+++ Data Structures/Graph/BreadthFirstSearch.java
@@ -0,0 +1,59 @@
+public class BreadthFirstSearch {
+    private static final int max = 101;
+    private static boolean[][] a = new boolean[max][max];
+    private static int[] trace = new int[max];
+    private static int s = 1;
+    private static int n = 8;
+    private static int f = 5;
+
+    private static void bfs() {
+        int[] queue = new int[max];
+        int front = 1;
+        int rear = 1;
+        queue[1] = s;
+        trace[s] = -1;
+
+        do {
+            int u = queue[front++];
+            for (int v = 1; v <= n; ++v) {
+                if (a[u][v] && trace[v] == 0) {
+                    queue[++rear] = v;
+                    trace[v] = u;
+                }
+            }
+        } while (front <= rear);
+    }
+
+    private static void result() {
+        System.out.println("From " + s + " you can visit:");
+        for (int v = 1; v <= n; ++v) {
+            if (trace[v] != 0) {
+                System.out.println(v);
+            }
+        }
+
+        System.out.println("The path from " + s + " to " + f + ":");
+        if (trace[f] == 0) {
+            System.out.println("Not found!");
+        } else {
+            while (f != s) {
+                System.out.print(f + " <- ");
+                f = trace[f];
+            }
+            System.out.print(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        a[1][2] = a[2][1] = true;
+        a[1][3] = a[3][1] = true;
+        a[2][3] = a[3][2] = true;
+        a[2][4] = a[4][2] = true;
+        a[3][5] = a[5][3] = true;
+        a[4][6] = a[6][4] = true;
+        a[7][8] = a[8][7] = true;
+        trace[s] = -1;
+        bfs();
+        result();
+    }
+}
